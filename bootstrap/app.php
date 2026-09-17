dacfcc7a61312b04c569e5ef85aa4b24e19cb19a
haaeell <haikaljawahir434@gmail.com>
2026-09-17 16:01:34 +0700

    ok

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Http\Request;
+use Sentry\Laravel\Integration;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
@@ -24,6 +25,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+
         $exceptions->render(function (PostTooLargeException $exception, Request $request) {
             $limit = ini_get('post_max_size') ?: 'batas server';
             $message = "Ukuran file yang dikirim terlalu besar. Maksimal tiap file 2 MB, dan batas total request server saat ini {$limit}. Kurangi ukuran file atau upload dokumen satu per satu.";
